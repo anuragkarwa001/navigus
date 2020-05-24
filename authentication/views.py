488548271905d2b776ae609c3a7c4fc000a9cca0
anurag <anuragkarwa001@gmail.com>
2020-05-24 11:47:40 +0530

    added image to login and register page created user authentication service using database created APIS for Handling login and signup request collected static files

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse 
+from django.template import RequestContext
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 # Create your views here.
 
 
+def login_page(request):
+    return render(request,"authentication/login.html")
+
+def signup_page(request):
+    return render(request,"authentication/registration.html")
 def login(request):
-    return render(request,'login.html')
+
+    username=request.POST['username']
+    password=request.POST['password']
     
+    user=auth.authenticate(username=username,password=password)
+    if user is not None:
+        auth.login(request,user)
+        return HttpResponse('<h1>sucess</h1>')
+    else:
+        messages.info(request,"invalid credentials")
+        return HttpResponse("failure")
+
+
+
+
+
+    return HttpResponse("hie")
+
 def signup(request):
-    return render(request,"registration.html")
+
+     
+     first_name=request.POST['name']
+     password=request.POST['password']
+     email=request.POST['iEmail']
+  
+     user=User.objects.create_user(username=email,password=password,email=email,first_name=first_name)
+     user.save()
+     return HttpResponse("user created")
 
 
